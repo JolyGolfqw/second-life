00e4e6608147376c0999d95647c40de21a63bdd3
sardalov <sarda10v@bk.ru>
2022-05-11 10:28:22 +0300

    newPage

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 import style from "./footer.module.css";
 import { Link } from "react-router-dom";
+import logoImg from "../../assets/logo.png";
 
 const Footer = () => {
-
   return (
     <footer>
       <div className={style.contentFoot}>
-        <div className={style.logo}>Хатико</div>
+        <div className={style.logo}>
+          <img src={logoImg} alt="logo"></img>
+          <span>Хатико</span>
+        </div>
         <div className={style.navigate}>
-            <Link to={"/"} className={style.listFoot}>Посещение приюта</Link>
-            <Link to={"/"} className={style.listFoot}>Питомцы</Link>
-            <Link to={"/"} className={style.listFoot}>Помощь приюту</Link>
-            <Link to={"/"} className={style.listFoot}>Вопросы и ответы</Link>
-            <Link to={"/"} className={style.listFootSingIn}>+7 (989) 000-00-00</Link>
+          <Link to={"/"} className={style.listFoot}>
+            Посещение приюта
+          </Link>
+          <Link to={"/"} className={style.listFoot}>
+            Питомцы
+          </Link>
+          <Link to={"/"} className={style.listFoot}>
+            Помощь приюту
+          </Link>
+          <Link to={"/"} className={style.listFoot}>
+            Вопросы и ответы
+          </Link>
+          <Link to={"/"} className={style.listFootSingIn}>
+            +7 (989) 000-00-00
+          </Link>
         </div>
       </div>
     </footer>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,20 +2,32 @@ import React from "react";
 import style from "./header.module.css";
 import { Link } from "react-router-dom";
 import ServicesDropdown from "./DropdownButton/ServicesDropdown";
-
+import logoImg from "../../assets/logo.png";
 const Header = () => {
-
   return (
     <header>
       <div className={style.contentHead}>
-        <div className={style.logo}>Хатико</div>
+        <div className={style.logo}>
+          <img src={logoImg} alt="logo"></img>
+          <span>Хатико</span>
+        </div>
         <div className={style.navigate}>
-            <Link to={"/pets"} className={style.listHead}>Найти питомца</Link>
-            <ServicesDropdown/>
-            <Link to={"/"} className={style.listHead}>Сборы</Link>
-            <Link to={"/animal-shelters"} className={style.listHead}>Помощь приютам</Link>
-            <Link to={"/"} className={style.listHead}>Советы</Link>
-            <Link to={"/"} className={style.listHeadSingIn}>Войти и Регистрация</Link>
+          <Link to={"/pets"} className={style.listHead}>
+            Найти питомца
+          </Link>
+          <ServicesDropdown />
+          <Link to={"/"} className={style.listHead}>
+            Сборы
+          </Link>
+          <Link to={"/animal-shelters"} className={style.listHead}>
+            Помощь приютам
+          </Link>
+          <Link to={"/"} className={style.listHead}>
+            Советы
+          </Link>
+          <Link to={"/"} className={style.listHeadSingIn}>
+            Войти и Регистрация
+          </Link>
         </div>
       </div>
     </header>
