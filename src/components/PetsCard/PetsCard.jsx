7ef8a32531f4091fc7486c2e0385931842afb4a3
sardalov <sarda10v@bk.ru>
2022-05-23 12:51:22 +0300

    front/(12:51 - 23.05.2022)

--- src/components/PetsCard/PetsCard.jsx
+++ src/components/PetsCard/PetsCard.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadPets } from "../../redux/features/pets";
+import PetsCardModal from "./PetsCardModal";
+import PetsCardRender from "./PetsCardRender";
 
 //!! ЖИВОТНЫЕ ИЗ ПРИЮТА
 const PetsCard = () => {
@@ -62,37 +64,8 @@ const PetsCard = () => {
           ) : (
             //!! КАРТОЧКА ПИТОМЦА
             <div className={style.cardPetsRender}>
-              {isShelteredPets.map((item) => {
-                return  (
-                  <div className={style.card} key={item._id}>
-                    <div className={style.headPetsCard}>
-                      <div className={style.agePets}>
-                        <span>{item.age}</span>
-                      </div>
-                      <div className={style.iconPets}>
-                        <div className={style.iconOne}>
-                          <ion-icon name="checkmark-outline"></ion-icon>
-                        </div>
-                        <div className={style.iconTwo}>
-                          <ion-icon name="file-tray-outline"></ion-icon>
-                        </div>
-                      </div>
-                    </div>
-                    <div className={style.bodyPetsCard}>
-                      <div className={style.imgPetsCard}>
-                        <img
-                          src={`http://localhost:4000/${item.img}`}
-                          alt="pet"
-                        ></img>
-                      </div>
-                      <div className={style.titlePetsCard}>{item.name}</div>
-                      <div className={style.descriptionPetsCard}>
-                        {item.description}
-                      </div>
-                    </div>
-                    <div className={style.footerPetsCard}></div>
-                  </div>
-                )
+              {isShelteredPets.map((item, index) => {
+                return <PetsCardRender name={item.name} description={item.description} gender={item.gender} img={item.img} age={item.age} key={index}/>;
               })}
             </div>
           )}
